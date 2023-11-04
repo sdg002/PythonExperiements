88578f6996f8a3184a9a70192f9e30e385897482
saurabh <saurabh_dasgupta@hotmail.com>
2023-11-04 23:49:54 +0000

    Got config_wrapper to work with 2 files

--- layered-config-parser/src/config/config_wrapper.py
+++ layered-config-parser/src/config/config_wrapper.py
@@ -1,8 +1,25 @@
 import configparser
+import os
+import logging
 
 class ConfigWrapper:
     def create_instance()->configparser.ConfigParser:
         print("Inside create_instance")
-        return None
+        config_parser = configparser.ConfigParser(os.environ,interpolation=configparser.ExtendedInterpolation())
+        dir_name = os.path.dirname( __file__)
+        base_ini_file = os.path.join(dir_name,"base_settings.ini")
+        if not os.path.exists(base_ini_file):
+            raise Exception(f"The base INI file '{base_ini_file}' was not found in this directory")
+        env = os.environ.get("environment", None)
+        logging.info(f"The environemnt variable 'environment' has the value '{env}'")
+        if env is None:
+            raise ValueError(f"The envrionment variable: 'environment' has not been set") 
+        environment_specific_ini_file = os.path.join(dir_name,"base_settings.ini")
+        if not os.path.exists(environment_specific_ini_file):
+            raise Exception(f"The environment specific INI file '{environment_specific_ini_file}' was not found in this directory")
+        config_parser.read(base_ini_file)
+        config_parser.read(environment_specific_ini_file)
+        return config_parser
+
     
     
--- layered-config-parser/src/scripts/oil_prices_scraper.py
+++ layered-config-parser/src/scripts/oil_prices_scraper.py
@@ -1,9 +1,13 @@
 #from config.config_wrapper import ConfigWrapper
 import sys
-print(sys.path)
+import logging
 from src.config import ConfigWrapper
 
 
 if __name__ =="__main__":
-    print("Inside oil prices scraper")
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Inside oil prices scraper")
     config_parser = ConfigWrapper.create_instance()
+    url=config_parser.get("oil","endpoint")
+    logging.info(f"Url={url}")
+
