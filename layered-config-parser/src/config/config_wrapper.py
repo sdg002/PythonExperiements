bf053fec3bee6339a63bfb6f067b0d03e017ea4e
saurabh <saurabh_dasgupta@hotmail.com>
2023-11-04 23:26:47 +0000

    Got basic ConfigWrapper creation in place, PYTHONPATH

---
+++ layered-config-parser/src/config/config_wrapper.py
@@ -0,0 +1,8 @@
+import configparser
+
+class ConfigWrapper:
+    def create_instance()->configparser.ConfigParser:
+        print("Inside create_instance")
+        return None
+    
+    
--- layered-config-parser/src/scripts/oil_prices_scraper.py
+++ layered-config-parser/src/scripts/oil_prices_scraper.py
@@ -1,3 +1,9 @@
+#from config.config_wrapper import ConfigWrapper
+import sys
+print(sys.path)
+from src.config import ConfigWrapper
+
 
 if __name__ =="__main__":
     print("Inside oil prices scraper")
+    config_parser = ConfigWrapper.create_instance()
