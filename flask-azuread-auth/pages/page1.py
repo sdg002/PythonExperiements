f34a9a7c461ded2f6a03d203d4c53d4e19dbda0b
saurabh <saurabh_dasgupta@hotmail.com>
2025-05-03 22:27:42 +0100

    Display username in Page1 and Page2

--- flask-azuread-auth/pages/page1.py
+++ flask-azuread-auth/pages/page1.py
@@ -2,6 +2,7 @@ import os
 import logging
 from datetime import datetime
 import dash
+import flask
 from dash import html
 
 dash.register_page(__name__, title=f'Page1 ({os.environ.get("ENVIRONMENT")})')
@@ -9,8 +10,9 @@ dash.register_page(__name__, title=f'Page1 ({os.environ.get("ENVIRONMENT")})')
 
 def layout():
     logging.info('Page handler:{__name__}')
+    user_name = flask.session["user"]["name"]
     html_elements = html.Div([
         html.H1('This is Page1'),
-        html.Div(f'This is our Page1 content. {datetime.now()}'),
+        html.Div(f'This is our Page1 content. {datetime.now()} {user_name=}'),
     ])
     return html_elements
--- flask-azuread-auth/pages/page2.py
+++ flask-azuread-auth/pages/page2.py
@@ -3,14 +3,16 @@ import logging
 from datetime import datetime
 import dash
 from dash import html
-
+import flask
 dash.register_page(__name__, title=f'Page2 ({os.environ.get("ENVIRONMENT")})')
 
 
 def layout():
     logging.info('Page handler:{__name__}')
+    user_name = flask.session["user"]["name"]
     html_elements = html.Div([
         html.H1('This is Page2'),
-        html.Div(f'This is our Page2 content. {datetime.now()}'),
+        html.Div(
+            f'This is our Page2 content. {datetime.now()} , {user_name=}'),
     ])
     return html_elements
