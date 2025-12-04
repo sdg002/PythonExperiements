65a0d795c45d3818729d1f473dde9d647adf46b1
saurabh <saurabh_dasgupta@hotmail.com>
2025-12-04 22:08:04 +0000

    create table

--- sqlmodeldemo/main.py
+++ sqlmodeldemo/main.py
@@ -1,7 +1,16 @@
 import sys
+from sqlmodel import SQLModel, create_engine
 from sqlmodeldemo.hero_model import Hero
+
+SQLITE_URL = "sqlite:///database.db"
+
+
+def init_db() -> None:
+    engine = create_engine(SQLITE_URL, echo=True)
+    SQLModel.metadata.create_all(engine, tables=[Hero.__table__])
+    # empty tables parameter - will not automatically create any table
 
 
 if __name__ == "__main__":
-    print(*sys.path, sep="\n")
+    init_db()
     print("This is the main module for sqlmodel.main")
