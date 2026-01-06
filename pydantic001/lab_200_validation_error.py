015143270b6f376a0147e35d6dfdd251f652acb8
saurabh <saurabh_dasgupta@hotmail.com>
2026-01-06 11:52:55 +0000

    Rename with lab_ , snake_case

--- pydantic001/lab_200_validation_error.py
+++ pydantic001/lab_200_validation_error.py
@@ -6,6 +6,9 @@ from pydantic import BaseModel
 
 
 class User(BaseModel):
+    """
+    A model representing a user with an ID, name, and active status.
+    """
     id: int
     name: str
     is_active: bool = True  # default value
